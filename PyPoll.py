9ef375afb1926bc0438ce939a7ebb9a4112d054a
hdolci <huydo670@gmail.com>
2021-07-03 11:18:43 -0700

    Added election analysis results to text file

--- PyPoll.py
+++ PyPoll.py
@@ -30,28 +30,41 @@ with open(file_to_load) as election_data:
             # Total number of votes each candidate received
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
+    # Saving results to text fil
+    with open(file_to_save, "w") as txt_file:
+        # Print the final vote count to the terminal.
+        election_results = (
+            f"\nElection Results\n"
+            f"-------------------------\n"
+            f"Total Votes: {total_votes:,}\n"
+            f"-------------------------\n")
+        print(election_results, end="")
+    # Save the final vote count to the text file.
+        txt_file.write(election_results)
 
-    # Percentage of votes each candidate won
-    for candidate_name in candidate_votes:
-        votes = candidate_votes[candidate_name]
-        vote_percentage = float(votes) / float(total_votes) * 100
-       # print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+        # Percentage of votes each candidate won
+        for candidate_name in candidate_votes:
+            votes = candidate_votes[candidate_name]
+            vote_percentage = float(votes) / float(total_votes) * 100
+            candidate_results = (
+                f"{candidate_name}: received {vote_percentage:.1f}% of the vote.\n")
+            # print(candidate_results)
+            txt_file.write(candidate_results)
 
-# The winner of the election based on popular vote
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
-            winning_count = votes
-            winning_percentage = vote_percentage
-            # And, set the winning_candidate equal to the candidate's name.
-            winning_candidate = candidate_name
-        print(f"\n{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            # The winner of the election based on popular vote
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                winning_count = votes
+                winning_percentage = vote_percentage
+                # And, set the winning_candidate equal to the candidate's name.
+                winning_candidate = candidate_name
+                #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-
-#  To do: print out the winning candidate, vote count and percentage to
-#   terminal.
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
+            #  To do: print out the winning candidate, vote count and percentage to
+            #   terminal.
+            winning_candidate_summary = (
+                f"-------------------------\n"
+                f"Winner: {winning_candidate}\n"
+                f"Winning Vote Count: {winning_count:,}\n"
+                f"Winning Percentage: {winning_percentage:.1f}%\n"
+                f"-------------------------\n")
+        txt_file.write(winning_candidate_summary)
